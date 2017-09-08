070d9948536841bae357eb47bcb4bcf578f3c061
Roman Stremedlovskyi <Roman_Stremedlovskyi@epam.com>
2017-09-08 21:41:39 +0300

    feat(fep-07) add wfu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
-class Calculator {}
+class Calculator {
+
+    sum(...args) {
+        return args.reduce((acc, curr) => {
+            return acc += curr;
+        })
+    }
+
+    substract( ...args) {
+        return args.slice().sort((a, b) => {
+            return a === b ? 0 : b - a;
+        }).reduce( (acc, curr) => {
+            return acc -= curr;
+        });
+
+
+        //return a > b ? a - b : b - a;
+    }
+
+    divide(a,b) {
+        if( b!== 0 ){
+            return a/b
+        } else {
+            throw new TypeError('Second number shouldnt be equal 0')
+        }
+    }
+
+
+
+    parse(str) {
+        return str
+            .split('+')
+            .map(el => el.split('-')
+                .reduce((acc, curr) =>curr > acc ? curr - acc: acc -curr))
+            .map(el => parseInt(el))
+        //.reduce((acc , curr) => acc += curr)
+
+    }
+    calc(str) {
+
+        return this.sum(...this.parse(str));
+
+    }
+}
+
+
+export const parse = (str) => {
+
+    return str
+        .split('+')
+        .map(el => el.split('-')
+            .reduce((acc, curr) =>curr > acc ? curr - acc: acc -curr))
+        .map(el => parseInt(el))
+        //.reduce((acc , curr) => acc += curr)
+
+}
 
 export default Calculator;
--- test/index.test.js
+++ test/index.test.js
@@ -2,18 +2,83 @@ import chai, {expect} from 'chai';
 import {spy, stub} from 'sinon';
 import sinonChai from 'sinon-chai';
 
+import {parse} from '../src/';
+
 import Calculator from '../src/';
 
 chai.use(sinonChai);
 
-describe('Calculator', function () {
+describe('Calculator', () => {
     let sut;
 
     beforeEach(() => {
         sut = new Calculator();
     });
 
-    it('should ', () => {
+    it('should sum numbers', () => {
+        const expected = 10;
+        const actual = sut.sum(1,2,3,4);
+        expect(expected).to.equal(actual);
+    })
 
-    });
+    it('should substract numbers', () => {
+        const expected = 1;
+        const actual = sut.substract(1, 2);
+        expect(expected).to.equal(actual);
+    })
+
+    it('should substract numbers', () => {
+        const expected = 0;
+        const actual = sut.substract(1, 2, 3);
+        expect(expected).to.equal(actual);
+    })
+
+    it('should divide two numbers', ()=>{
+        const expected = -10;
+        const actual = sut.divide(-100,10)
+        expect(expected).to.equal(actual)
+    })
+
+    it('should throw error if second number is equal 0', ()=>{
+        const actual = sut.divide.bind(null,-100,0)
+        expect(actual).to.throw()
+    })
+
+    it('should calc pssed string', ()=>{
+        const actual = sut.calc('1+2+3+4');
+        const expected = 10;
+        expect(expected).to.equal(actual);
+    })
+
+    //it('should calc pssed string', ()=>{
+    //    const actual = sut.calc('1+2+3+4-10');
+    //    const expected = 0;
+    //    expect(expected).to.equal(actual);
+    //})
+
+    it('should calc pssed string', ()=>{
+
+        spy(sut, 'parse')/*.returns([10, 2])*/;
+
+        const actual = sut.calc('10+2');
+        const expected = 12;
+        expect(expected).to.equal(actual);
+    })
+
 });
+
+
+describe('Parse', () => {
+    let sut;
+    beforeEach(() => {
+        sut = parse;
+    })
+
+    it('Should parse str and array', () => {
+
+        const actual = sut('10+2');
+        const expected = ([10, 2]);
+
+        expect(expected).to.have.all.members(actual);
+    })
+})
