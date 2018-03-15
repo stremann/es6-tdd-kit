c4b7d482fe5780a6fcbe44de04bf6406eeb40914
Roman Stremedlovskyi <Roman.Stremedlovskyi@ticketmaster.com>
2018-03-15 12:07:18 +0200

    feat(fep-08) add fep-08 workshop

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
-class Calculator {}
+// import fetch from 'whatwg-fetch';
+function isNumbers(args) {
+    return args.every(el => typeof el === 'number');
+}
+
+class Calculator {
+    sum(...args) {
+
+        if(!isNumbers(args)){
+            throw new Error('Input valid arguments')
+        }
+        return args.reduce((a, b) => a + b);
+    }
+
+    divide(a, b) {
+        if (b == 0) {
+            throw new Error('Do not divide on zero');
+        } else {
+            return a / b;
+        }
+    }
+
+    sqrt(a) {
+        return Math.sqrt(a)
+    }
+
+    pow(a, b) {
+        return Math.pow(a, b);
+    }
+
+    hyp(a, b) {
+        return this.sqrt(this.sum(this.pow(a,2),this.pow(b,2)));
+    }
+
+    getPI() {
+       return fetch('server/pi')
+           .then(response => response.json());
+
+    }
+
+
+}
 
 export default Calculator;
--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,7 @@ import chai, {expect} from 'chai';
 import {spy, stub} from 'sinon';
 import sinonChai from 'sinon-chai';
 
-import Calculator from '../src/';
+import Calculator from '../src';
 
 chai.use(sinonChai);
 
@@ -13,7 +13,63 @@ describe('Calculator', function () {
         sut = new Calculator();
     });
 
-    it('should ', () => {
-
+    it('should return sum of a and b', () => {
+        const a = 5, b = 3;
+        expect(sut.sum(a, b)).to.equal(8);
     });
+
+
+
+    it('should handle unexpected strings in args', () => {
+       expect(()=>{sut.sum(1.5, 'hello', 4, 10, null, {})}).to.throw();
+    });
+
+
+
+    it('should divide 2 numbers', () => {
+        expect(sut.divide(8, 2)).to.equal(4);
+    });
+
+    it('should return error on zero divide', () => {
+        expect(function () {
+                sut.divide(8, 2)
+            }
+        ).to.not.throw();
+
+
+        expect(() => {
+            sut.divide(8, 0)
+        }).to.throw()
+    });
+
+    it('should return sqrt', () => {
+        expect(sut.sqrt(16)).to.equal(4);
+    });
+
+    it('should handle different amount of arguments', () => {
+        expect(sut.sum(1, 2, 3, 4)).to.equal(10);
+    });
+    it('should return a powered by b', () => {
+        expect(sut.pow(8, 2)).to.equal(64);
+    });
+    it('should calculate hyp', () => {
+        expect(sut.hyp(3, 4)).to.equal(5);
+    });
+
+
+    it('should calc hyp', () => {
+        // spy(sut,'sum');
+        // stub(sut,'sum');
+        stub(sut, 'sum').withArgs(9, 16).returns(25);
+        sut.hyp(3, 4);
+        expect(sut.sum).calledWith(9, 16);
+
+        expect(sut.hyp(3, 4)).to.equal(5);
+    });
+
+    xit('should return PI from server', () => {
+        stub(fetch);
+       expect(sut.getPI()).to.equal(Math.PI);
+    });
+
 });
